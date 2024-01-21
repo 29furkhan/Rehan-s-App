d34a03943bd76b50ddc6836f89e82c9380729cd5
furkhan1999 <me.furkhan.shaikh@gmail.com>
2024-01-21 20:56:53 +0530

    added inital design to the project

---
+++ my_app/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const router = require('./routes/router')
+const app = express()
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json())
+app.use(cors())
+app.use(router)
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+
+app.listen(8080, () => {
+    console.log("Connected to backend.")
+})
