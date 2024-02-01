e25d3c4ef917f0919e2392ccd97f9c484d4c2ee8
Furkhan Shaikh <53703371+29furkhan@users.noreply.github.com>
2024-02-01 07:05:46 +0530

    Update App.jsx
    Changed URL of backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Content from './components/Content';
 
 function App() {
-  const core_url = "http://localhost:8080/patients"
+  const core_url = "https://rehan-s-app-backend.vercel.app/patients"
 
   return (
     <>
